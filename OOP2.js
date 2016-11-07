5250d4a90079745fe7dfa6261f2e68e69bd9c15f
Robert Dunn <robertdunn220@gmail.com>
2016-11-07 10:53:25 -0500

    Stuff

---
+++ OOP2.js
@@ -0,0 +1,104 @@
+// function like() {
+//   this.timesLiked++;
+// }
+//
+// function hate() {
+//   this.timesLiked--;
+// }
+//
+// var pic1 = {
+//   url: 'lolcat.jpg',
+//   timesLiked: 0,
+// };
+//
+// var pic2 = {
+//   url: 'chucknorris.jpg',
+//   timesLiked: 0
+// };
+// like.apply(pic1);
+// like.apply(pic1);
+// like.apply(pic2);
+//
+// console.log(pic1.timesLiked);
+// console.log(pic2.timesLiked);
+//
+// hate.apply(pic1);
+// hate.apply(pic1);
+// hate.apply(pic1);
+//
+// console.log(pic1.timesLiked);
+
+
+// var mom = {
+//   firstName: 'Alice',
+//   lastName: 'Wong',
+//   eyeColor: 'brown',
+//   hairColor: 'black',
+//   showInfo: function () {
+//     for (var prop in this) {
+//       console.log(prop + ': ' + this[prop]);
+//     }
+//   }
+// };
+// var daughter = {
+//   firstName: 'Ilene',
+//   hairColor: 'brown'
+// };
+//
+// daughter.__proto__ = mom;
+// console.log(daughter.lastName);
+//
+// mom.showInfo();
+// daughter.showInfo();
+
+// Four
+// function Character () {
+// }
+//
+// Character.prototype.attack = function(enemy) {
+//   enemy.health -= this.power;
+// };
+//
+// function Hero() {
+//   this.name = 'Hero';
+//   this.health = 10;
+//   this.power = 5;
+// }
+//
+//   Hero.prototype = Object.create(Character.prototype);
+//
+//   Hero.prototype.attack = function(enemy) {
+//     randNum = Math.floor(Math.random() * 10) + 0;
+//     if (randNum > 2) {
+//       enemy.health -= this.power;
+//     }
+//     else if (randNum <= 2){
+//       power = this.power * 2;
+//       enemy.health -= power;
+//     }
+//   };
+//
+//   function Enemy() {
+//     this.name = 'Enemy';
+//     this.health = 6;
+//     this.power = 2;
+//   }
+//
+//   Enemy.prototype = Object.create(Character.prototype);
+//
+// var hero = new Hero();
+// var enemy = new Enemy();
+// hero.attack(enemy);
+// console.log(enemy.health);
+
+// Six
+function Person(name) {
+  this.name = name;
+  greet = function(other) {
+    console.log('Hello ' + other.name + '! My name is ' + this.name + '.');
+  };
+}
+
+var pOne = new Person('Rob');
+var pTwo = new Person('Riss');
+greet.apply(pOne, [pTwo]);
